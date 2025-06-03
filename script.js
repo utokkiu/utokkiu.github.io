c2d71a047fa35ed0269cf58822bb38cc6106649b
utokkiu <apri8898@gmail.com>
2025-06-03 23:36:19 +0700

    tambah

---
+++ script.js
@@ -0,0 +1,7 @@
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function () {
+  navBar = document.querySelector(".nav-bar");
+  navBar.classList.toggle("active");
+};
+
+feather.replace();
